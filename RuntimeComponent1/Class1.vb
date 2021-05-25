f86c4cc4b9ff0cf90068cf845110bacf78a46a45
_Helper <sollohypertexr@gmail.com>
2021-05-25 15:39:18 +0200

    index on !000: 27671a2 WIP on 000: d03525c Revert "Revert "Revert "Přidejte soubory projektu."""

---
+++ RuntimeComponent1/Class1.vb
@@ -0,0 +1,8 @@
+﻿<DebuggerDisplay("{GetDebuggerDisplay(),nq}")>
+Public NotInheritable Class Class1
+    Private ReadOnly Property DebuggerDisplay As String
+        Get
+            Return ToString()
+        End Get
+    End Property
+End Class="ftp://home.nic/"()
