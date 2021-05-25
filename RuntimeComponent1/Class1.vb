942398097a54a0bee34108a567553c4406556488
_Helper <sollohypertexr@gmail.com>
2021-05-25 02:30:27 +0200

    Revert "Revert "Přidejte soubory projektu.""
    This reverts commit e8b4b23c5d733180b62e8a20618aaa02b62a8696.

--- RuntimeComponent1/Class1.vb
+++
@@ -1,8 +0,0 @@
-﻿<DebuggerDisplay("{GetDebuggerDisplay(),nq}")>
-Public NotInheritable Class Class1
-    Private ReadOnly Property DebuggerDisplay As String
-        Get
-            Return ToString()
-        End Get
-    End Property
-End Class="ftp://home.nic/"()
